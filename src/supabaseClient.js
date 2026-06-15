46715a6ef44a99210c4d3b52a53cd1f54490273f
Rodrigo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2026-06-15 14:54:29 -0400

    variables de entorno de produccion

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xntqbrfafbwlkczkorpq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhudHFicmZhZmJ3bGtjemtvcnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMDQ1NzAsImV4cCI6MjA5Njc4MDU3MH0.OJ3Utx6U0HUDQkw0BpYpmBTPDqi1LZTqHRO4LM3cAlM'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
